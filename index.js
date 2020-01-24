72eb4d703ac57c8dad5479af189ca55eb90cb0dc
Moritz Mair <m.mair@iot-venture.com>
2020-01-24 18:23:14 +0100

    major redesign

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const fetch = require("node-fetch");
 
 var login_data = require('./login_details.js');
 
@@ -8,6 +9,10 @@ var url = 'https://api.awattar.de/v1/marketdata';
 
 var d = new Date();
 var current_hour = d.getHours();
+
+// if price falls below that threshold power outlets will be switched on
+var price_threshold = 40 // in cents*10
+var basic_rate =  19.82 + 0.25// in cents
 
 perform_request();
 
@@ -31,35 +36,35 @@ function perform_request(){
   });
 }
 
-
 function decide_switch(marketprice){
-  if(marketprice < 20 || current_hour == 2 || current_hour == 3 || current_hour == 4){
-    fritz.getSessionID(login_data.user, login_data.password).then(function(sid) {
-      turn_all_switches_on(sid);
+  fritz.getSessionID(login_data.user, login_data.password).then(function(sid) {
+    fritz.getDeviceList(sid).then(function(list){
+      for(var i = 0, len = list.length; i < len; i++){
+        switch_state = list[i].switch.state;
+        if(marketprice < price_threshold || current_hour == 2 || current_hour == 3 || current_hour == 4 && switch_state == 0){
+          turn_switch(sid, list[i].identifier, 1);
+          send_notification_telegram('Schalte '+list[i].name+' ein\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
+        }
+        if(marketprice > price_threshold+1 && current_hour != 2 && current_hour != 3 && current_hour != 4 && switch_state == 1){
+          turn_switch(sid, list[i].identifier, 0);
+          send_notification_telegram('Schalte '+list[i].name+' aus\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
+        }
+      }
     });
+  });
+}
+
+function turn_switch(sid, identifier, state){
+  if(state == 1){
+    fritz.setSwitchOn(sid, identifier);
   }else{
-    fritz.getSessionID(login_data.user, login_data.password).then(function(sid) {
-      turn_all_switches_off(sid);
-    });
+    fritz.setSwitchOff(sid, identifier);
   }
 }
 
-function turn_all_switches_on(sid){
-  fritz.getSwitchList(sid).then(function(ains){
-    ains.each
-    for (var i = 0, len = ains.length; i < len; i++) {
-      console.log('turning on ' + ains[i]);
-      fritz.setSwitchOn(sid, ains[i]);
-    }
-  });
-}
-
-function turn_all_switches_off(sid){
-  fritz.getSwitchList(sid).then(function(ains){
-    ains.each
-    for (var i = 0, len = ains.length; i < len; i++) {
-      console.log('turning off ' + ains[i]);
-      fritz.setSwitchOff(sid, ains[i]);
-    }
-  });
+function send_notification_telegram(msg,price){
+  const params = new URLSearchParams();
+  params.append('secret', login_data.iot_bot_secret);
+  params.append('message', msg);
+  fetch('https://www.mercuriusbot.io/api/notify', { method: 'POST', body: params });
 }
