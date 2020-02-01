1b90d59f74a4140b5a8d1ac9dd9356243ee09189
Moritz Mair <m.mair@iot-venture.com>
2020-02-01 18:00:42 +0100

    turning on of plug can now be configured to be on in the cheapest hours

--- index.js
+++ index.js
@@ -25,14 +25,16 @@ var url = 'https://api.awattar.de/v1/marketdata';
 var d = new Date();
 var current_hour = d.getHours();
 
-var price_threshold = config_file.price_threshold * 10
-var basic_rate =  config_file.basic_rate
+var basic_rate =  config_file.basic_rate;
+var price_threshold = config_file.always_turn_on_below*10;
+
+var epex_data = new Object();
 
 
 
-perform_request();
-
-setInterval(function(){ perform_request(); }, 12*1000*60);
+refresh_epex();
+setInterval(function(){ refresh_epex(); }, 1000*60*60*5);
+setInterval(function(){ decide_switch(epex_data); }, 1000*60);
 
 function perform_request(){
   https.get(url, function(res){
@@ -57,23 +59,73 @@ function decide_switch(awattar_response){
   fritz.getSessionID(config_file.fritzboxuser, config_file.fritzboxpassword).then(function(sid) {
     fritz.getDeviceList(sid).then(function(list){
       server.refresh_parameters(list, awattar_response)
+      cheapest_hours = identify_cheapest_hours();
       for(var i = 0, len = list.length; i < len; i++){
         switch_state = list[i].switch.state;
-        if((marketprice < price_threshold || current_hour == 2 || current_hour == 3 || current_hour == 4) && switch_state == 0){
+        if((marketprice < price_threshold || cheapest_hours.includes(current_hour)) && switch_state == 0){
           turn_switch(sid, list[i].identifier, 1);
-          send_notification_telegram('Schalte '+list[i].name+' ein\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
+          send_notification_telegram('Lade in folgenden Stunden: '+cheapest_hours.join(', ')+'\nSchalte '+list[i].name+' ein\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
           console.log('switched on '+list[i].name);
-          decide_switch(awattar_response);
         }
-        else if((marketprice > price_threshold+1 && current_hour != 2 && current_hour != 3 && current_hour != 4) && switch_state == 1){
+        else if((marketprice > price_threshold+1 && !cheapest_hours.includes(current_hour)) && switch_state == 1){
           turn_switch(sid, list[i].identifier, 0);
-          send_notification_telegram('Schalte '+list[i].name+' aus\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
+          send_notification_telegram('Lade in folgenden Stunden: '+cheapest_hours.join(', ')+'\nSchalte '+list[i].name+' aus\nPreis pro KWH: '+(marketprice/10+basic_rate)+' Cent\nMarktpreis pro KWH: '+(marketprice/10)+' Cent\nTemperatur '+list[i].temperature.celsius/10+' °C');
           console.log('switched off '+list[i].name);
-          decide_switch(awattar_response);
         }
       }
     });
   });
+}
+
+function refresh_epex(){
+  https.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+      body += chunk;
+    });
+
+    res.on('end', function(){
+      var response = JSON.parse(body);
+      console.log("Got a new epec data: ", response.data);
+      epex_data = response;
+    });
+  }).on('error', function(e){
+    console.log("Got an error: ", e);
+  });
+}
+
+function identify_cheapest_hours(){
+  cheapest_hours = new Array();
+  remaining_epex = new Array();
+  //get timestamp of next event where battery needs to be full
+  var needs_to_be_full = new Date();
+  if(d.getHours() <= config_file.turn_on_until_24h){
+    needs_to_be_full.setHours(config_file.turn_on_until_24h);
+  }else{
+    needs_to_be_full.setHours(config_file.turn_on_until_24h);
+    needs_to_be_full.setDate(needs_to_be_full.getDate() + 1);
+  }
+
+  console.log("needs to be full at " + needs_to_be_full)
+  
+  for(var i = 0, len = epex_data.data.length; i < len; i++){
+    if(epex_data.data[i].start_timestamp < needs_to_be_full && epex_data.data[i].start_timestamp > config_file.hours_to_turn_on_after){
+      var date = new Date(epex_data.data[i].start_timestamp);
+      remaining_epex.push(epex_data.data[i])
+    }
+  }
+
+  remaining_epex = remaining_epex.sort(function(a, b){return a.marketprice - b.marketprice});
+
+  for(var i= 0; i < config_file.hours_to_turn_on; i++){
+    var cheap_date = new Date(remaining_epex[i].start_timestamp)
+    cheapest_hours.push(cheap_date.getHours());
+  }
+
+  console.log(cheapest_hours)
+
+  return cheapest_hours
 }
 
 function turn_switch(sid, identifier, state){
